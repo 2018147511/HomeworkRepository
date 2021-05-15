99f89bb52e44a1c138eb9bdba8cb164d91295d01
2018147511 <railgun2015@gmail.com>
2021-05-16 02:20:57 +0900

    LAB4

--- LAB4/Icecream-script.js
+++ LAB4/Icecream-script.js
@@ -146,6 +146,7 @@ function initialize(products) {
     const more_info = document.createElement('p');
     more_info.classList.add('goods_btn');
     more_info.textContent= '더 알아보기';
+    more_info.setAttribute('z-index', 1000);
 
     // 색션에 해당 제품의 타입을 클래스로 지정
     section.setAttribute('class', product.type);
